66e304cc3bd827ad2fd24a40b9e8c1b2d6804ae5
GDalonso <guicastro.200@gmail.com>
2022-10-08 02:26:58 -0300

    endereço e horario adicionados

--- src/api/team-api.js
+++ src/api/team-api.js
@@ -1,5 +1,15 @@
-export const createTeam = async ({ name, descricao }) => {
-  console.log(name)
-  console.log(descricao)
+export const createTeam = async ({ name, descricao, endereco, horario }) => {
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+
+  sleep(200).then(() => {
+    console.log(name)
+    console.log(descricao)
+    console.log(endereco)
+    console.log(horario)
+
+  })
+
   return name // function needs to return something
 }
--- src/screens/TeamScreen.js
+++ src/screens/TeamScreen.js
@@ -7,26 +7,38 @@ import Paragraph from '../components/Paragraph'
 import { nameValidator } from '../helpers/nameValidator'
 import { createTeam } from '../api/team-api'
 import TextInput from '../components/TextInput'
+import Toast from '../components/Toast'
 
 export default function TeamScreen({ navigation }) {
   const [name, setName] = useState({ value: '', error: '' })
   const [descricao, setDescricao] = useState({ value: '', error: '' })
+  const [endereco, setEndereco] = useState({ value: '', error: '' })
+  const [horario, setHorario] = useState({ value: '', error: '' })
 
   const [loading, setLoading] = useState()
   const [error, setError] = useState()
 
   const onTeamCreatePressed = async () => {
+    // field cannot be empty validators for now
+    // actual validators still to be implemented
     const nameError = nameValidator(name.value)
     const descricaoError = nameValidator(descricao.value)
-    if (nameError || descricaoError) {
+    const enderecoError = nameValidator(endereco.value)
+    const horarioError = nameValidator(horario.value)
+
+    if (nameError || descricaoError || enderecoError || horarioError) {
       setName({ ...name, error: nameError })
       setDescricao({ ...descricao, error: descricaoError })
+      setEndereco({ ...endereco, error: enderecoError })
+      setHorario({ ...horario, error: horarioError })
       return
     }
     setLoading(true)
     const response = await createTeam({
       name: name.value,
       descricao: descricao.value,
+      endereco: endereco.value,
+      horario: horario.value,
     })
     if (response.error) {
       setError(response.error)
@@ -47,16 +59,33 @@ export default function TeamScreen({ navigation }) {
         errorText={name.error}
       />
       <TextInput
-        label="Descricao"
+        label="Descrição"
         returnKeyType="next"
         value={descricao.value}
         onChangeText={(text) => setDescricao({ value: text, error: '' })}
         error={!!descricao.error}
         errorText={descricao.error}
       />
-      <Button mode="outlined" onPress={onTeamCreatePressed}>
+      <TextInput
+        label="Endereço"
+        returnKeyType="next"
+        value={endereco.value}
+        onChangeText={(text) => setEndereco({ value: text, error: '' })}
+        error={!!endereco.error}
+        errorText={endereco.error}
+      />
+      <TextInput
+        label="Horário"
+        returnKeyType="next"
+        value={horario.value}
+        onChangeText={(text) => setHorario({ value: text, error: '' })}
+        error={!!horario.error}
+        errorText={horario.error}
+      />
+      <Button mode="outlined" loading={loading} onPress={onTeamCreatePressed}>
         Criar Time
       </Button>
+      <Toast message={error} onDismiss={() => setError('')} />
     </Background>
   )
 }
