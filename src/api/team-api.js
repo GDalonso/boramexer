ae77164771434f1519d80d91d9bccac929b4c5ed
GDalonso <guicastro.200@gmail.com>
2022-10-08 02:54:48 -0300

    testando-conn-banco

--- src/api/team-api.js
+++ src/api/team-api.js
@@ -1,9 +1,32 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+// import firebase from 'firebase/app'
+
 export const createTeam = async ({ name, descricao, endereco, horario }) => {
   function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms))
   }
+  // Initialize Realtime Database and get a reference to the service
 
   try {
+    // const app =
+    //   getApps().length === 0 ? initializeApp(FIREBASE_CONFIG) : getApp()
+    // const database = getDatabase(app)
+    const database = getDatabase()
+    // firebase.database().ref('times/1').set({
+    //   name,
+    //   descricao,
+    //   endereco,
+    //   horario,
+    // })
+
+    set(ref(database, 'times/1'), {
+      name,
+      descricao,
+      endereco,
+      horario,
+    })
+
     sleep(200).then(() => {
       console.log(name)
       console.log(descricao)
