a50fe62a1317361c2c20f1eae2a81482773163b1
GDalonso <guicastro.200@gmail.com>
2022-10-09 00:03:59 -0300

    adding css to the TeamList component

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-paper'
+import './team_card.css'
+import Button from './Button'
+import { theme } from '../core/theme'
 
 export default function TeamCard({ nome, descricao, endereco, horario }) {
   return (
-    <div>
-      <Text style={styles.text}> {nome} </Text>
-      <Text style={styles.text}> {descricao} </Text>
-      <Text style={styles.text}> {endereco} </Text>
-      <Text style={styles.text}> {horario} </Text>
+    <div className="team_card">
+      <Text style={styles.titleText}>{nome}</Text>
+      <br />
+      <br />
+      <Text style={styles.regularText}> {descricao} </Text>
+      <br />
+      <Text style={styles.regularText}> {endereco} </Text>
+      <Text style={styles.regularText}> {horario} </Text>
+      <Button mode="contained" onPress={() => console.log('aa')}>
+        Pedir para participar
+      </Button>
     </div>
   )
 }
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 15,
+  titleText: {
+    fontSize: 20,
+    color: theme.colors.text,
+    lineHeight: 21,
+    textAlign: 'center',
+    marginBottom: 12,
+  },
+  regularText: {
+    fontSize: 16,
+    color: theme.colors.primary,
     lineHeight: 21,
     textAlign: 'center',
     marginBottom: 12,
